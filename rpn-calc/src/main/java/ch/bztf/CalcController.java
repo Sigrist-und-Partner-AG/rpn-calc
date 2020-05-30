939ef3d2f545274bd5e4fd0e7612206d72d322d8
Timothy Schmid <86836531+Sigrist-und-Partner-AG@users.noreply.github.com>
2020-05-30 11:12:12 +0200

    Connect controller to fxml

--- rpn-calc/src/main/java/ch/bztf/CalcController.java
+++ rpn-calc/src/main/java/ch/bztf/CalcController.java
@@ -1,6 +1,33 @@
 package ch.bztf;
 
-/*
-public class CalcController extends Application {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ * @author Timothy R. Schmid
+ * 
+ * JavaFX utilizes an MVC (model-view-controller)
+ * architecture. This class is the controller for
+ * the calculator represented by the fxml.
+ */
+public class CalcController {
+
+    /**
+     * The free-form text field into which the RPN
+     * expression is entered. Validation is performed
+     * only as soon as the result is requested.
+     */
+    @FXML
+    private TextField input;
+
+    @FXML
+    private void appendToInput(ActionEvent ev) {
+        Button button = (Button)ev.getSource();
+        input.appendText(button.getText() + " ");
+    }
+
+    // appendDigitToInput
+    // appendOperatorToInput
 }
-*/
--- rpn-calc/src/main/java/module-info.java
+++ rpn-calc/src/main/java/module-info.java
@@ -2,4 +2,5 @@ module ch.bztf {
     requires transitive javafx.controls;
     requires javafx.fxml;
     exports ch.bztf;
+    opens ch.bztf to javafx.fxml;
 }
