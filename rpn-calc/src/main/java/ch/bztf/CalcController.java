87bb42e1d6cc519c96c02df82392f6d9a6f98e3d
Timothy Schmid <86836531+Sigrist-und-Partner-AG@users.noreply.github.com>
2020-06-01 17:43:52 +0200

    Code fxml deletion hooks

--- rpn-calc/src/main/java/ch/bztf/CalcController.java
+++ rpn-calc/src/main/java/ch/bztf/CalcController.java
@@ -4,6 +4,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.robot.Robot;
 
 /**
  * @author Timothy R. Schmid
@@ -21,6 +23,73 @@ public class CalcController {
      */
     @FXML
     private TextField input;
+
+    /**
+     * Wipes the input field completely.
+     * 
+     * @param ev The action event.
+     *           It is unused by this function.
+     */
+    @FXML
+    private void clearInput(ActionEvent ev) {
+        input.setText("");
+        input.requestFocus();
+    }
+
+    /**
+     * Deletes the character that precedes the current
+     * caret position from the input text field.
+     * If a selection is active, the selected text
+     * is deleted in its entirety instead.
+     * <p>
+     * Note: This is equivalent to pressing the
+     *       "backspace" key on the keyboard.
+     * </p>
+     * 
+     * @param ev The action event.
+     *           It is unused by this function.
+     */
+    @FXML
+    private void truncateInputByChar(ActionEvent ev) {
+        input.deletePreviousChar();
+    }
+
+    /**
+     * Deletes the string that precedes the current
+     * caret position from the input text field,
+     * delimited by whitespace or the start of text.
+     * If a selection is active, the selected text
+     * is deleted in its entirety instead.
+     * <p>
+     * Note: This is equivalent to pressing the key
+     *       combination "control" + "backspace" on
+     *       the keyboard.
+     * </p>
+     * @implNote This function sends virtual keystrokes
+     *           to the calculator by instantiating a
+     *           {@code Robot}. This was simpler than
+     *           attempting to re-code the built-in
+     *           functionality. This may fail if the
+     *           appropriate security manager permission
+     *           hasn't been set, in which case this
+     *           function becomes a no-op.
+     * 
+     * @param ev The action event.
+     *           It is unused by this function.
+     */
+    @FXML
+    private void truncateInputByWord(ActionEvent ev) {
+        try {
+            Robot r = new Robot();
+            r.keyPress(KeyCode.CONTROL);
+            r.keyPress(KeyCode.BACK_SPACE);
+            r.keyRelease(KeyCode.BACK_SPACE);
+            r.keyRelease(KeyCode.CONTROL);
+        } catch (SecurityException e) {
+            System.err.println("Warning: Permission 'createRobot' not granted");
+        }
+    }
+
 
     @FXML
     private void appendToInput(ActionEvent ev) {
